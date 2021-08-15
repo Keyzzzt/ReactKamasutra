2a27ebbeeff3bace4d749bd4b8c372ebbe511025
Keizt <keyzzzt@gmail.com>
2021-08-15 22:35:29 +0300

    Changed Users functional component to a class component. Added ajax request in componentDidMount method.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
     <nav className={s.nav}>
       <div><NavLink className={s.item} to="/profile" activeClassName={s.active}>Profile</NavLink></div>
       <div><NavLink className={s.item} to="/dialogs" activeClassName={s.active}>Dialogs</NavLink></div>
+      <div><NavLink className={s.item} to="/users" activeClassName={s.active}>Users</NavLink></div>
       <div><NavLink className={s.item} to="/news" activeClassName={s.active}>News</NavLink></div>
       <div><NavLink className={s.item} to="/music" activeClassName={s.active}>Music</NavLink></div>
       <div><NavLink className={s.item} to="/settings" activeClassName={s.active}>Settings</NavLink></div>
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
+import * as axios from "axios";
+import styles from '../../styles/Users.module.css'
+import noFoto from '../../assets/images/noLogo.png'
 
-const Users = (props) => {
-    if(props.users.length === 0){
-        props.setUsers([
-            { id: 1, name: 'Bond', status: '007', location: { country: 'USA', city: "LA"}, follow: true},
-            { id: 2, name: 'James', status: '007', location: { country: 'UK', city: "London"}, follow: true},
-            { id: 3, name: 'James Bond', status: '007', location: { country: 'France', city: "Paris"}, follow: false},
-        ])
+class Users extends React.Component{
+    componentDidMount() {
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response =>{
+                this.props.setUsers(response.data.items)
+            })
+
     }
-    const followUnfollowHandler = (userId) => props.followUnfollow(userId)
+    followUnfollowHandler = (userId) => this.props.followUnfollow(userId)
 
-
-
-    return (
-        <div>
-            {props.users.map(user => {
-            return (
-                <div>
-                    <div>ID: {user.id}</div>
-                    <div>Name: {user.name}</div>
-                    <div>Status: {user.status}</div>
-                    <div>Location:
-                        <div>City: {user.location.city} Country:  {user.location.country}</div>
-                    </div>
-                    <button onClick={() => followUnfollowHandler(user.id)}>{user.follow ? 'UnFollow' : 'Follow'}</button>
-                </div>
-            )
-        })}</div>
-    )
+    render(){
+        return (
+            <div >
+                {this.props.users.map(user => {
+                    return (
+                        <div className={styles.user}>
+                            <div >
+                                <img className={styles.userPhoto} src={user.photos.small ? user.photos.small : noFoto} alt=""/>
+                            </div>
+                            <div>
+                                <div>ID: {user.id}</div>
+                                <div>Name: {user.name}</div>
+                                <button onClick={() => this.followUnfollowHandler(user.id)}>{user.follow ? 'UnFollow' : 'Follow'}</button>
+                            </div>
+                        </div>
+                    )
+                })}</div>
+        )
+    }
 }
 
 export default Users
