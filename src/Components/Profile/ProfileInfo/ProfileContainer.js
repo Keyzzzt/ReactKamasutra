021d58bdb1de385026e876466994a5dc71da4ebc
Keizt <keyzzzt@gmail.com>
2021-08-22 21:09:09 +0300

    Added withRouter, check ProfileContainer.jsx

--- src/Components/Profile/ProfileInfo/ProfileContainer.js
+++ src/Components/Profile/ProfileInfo/ProfileContainer.js
@@ -3,11 +3,16 @@ import Profile from "../Profile";
 import axios from "axios";
 import {setUserProfile} from "../../../redux/reducers/profileReducer";
 import {connect} from "react-redux";
+import {withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component{
     componentDidMount() {
+        let userId = this.props.match.params.userId
+        if(!userId){
+            userId = 2
+        }
         axios
-            .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+            .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
             .then(response =>{
                 console.log(response)
                 this.props.setUserProfile(response.data)
@@ -27,4 +32,7 @@ const mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer)
+let ProfileContainerWithURL = withRouter(ProfileContainer)    // Теперь в ProfileContainerWithURL будут данные из URL - match, location, history.
+
+
+export default connect(mapStateToProps, {setUserProfile})(ProfileContainerWithURL)
